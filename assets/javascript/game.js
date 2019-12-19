d2ac939926169baf55431c2ee9b6b2242c632ad4
Mike Nezzer <mnezz1131@gmail.com>
2019-12-19 15:08:10 -0600

    I suck so bad at javascript it's depressing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,10 @@
 //3. Guess what letter I'm thinking of
-
 //4. Wins: (# of times the user has guessed the letter correctly)
-
 //5. Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-
-//6. Guesses Left: (# of guesses left. This will update)
-
+//6. Guesses Left: (# of guesses left. This will update) done
 //7. Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-
 //8. When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-
 //9. When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
-
 
 ///=Gives the computer choices to pick from
 var letters = ["a,b,c,d"]
@@ -24,46 +17,68 @@ var losses = 0;
 var guessesLeft = 5;
 
 //Guesslesft
+// I am setting a var "guessed" to a function that goes to my html document.
+// .querySelector find the first element from the html doc with the ID of guesses.
+//.innerHTML is going to let me replace the contents on the html with the new updated
+//content.
+//The updated content will be equal to the var guessesLeft
+//This will update each time when called by subtrating -1 from guessesLeft.
 var guessed = function(){
 document.querySelector("#guesses").innerHTML = guessesLeft;
 }  
 
+//updateguess I don't understand why I need update guess and update guesses so far??
 
 
-//updateguess
+//updateguesesssofar  
+// I am setting a var guessedSoFar to a function that goes to my html document
+//.querySelector will find the first element from html with ID of guesses
+//.innerhtml will up update content in guessessofar
+//The updated content should be the letters the user is guessing/entering
+//I think I need to push the userGuess to the array but don't know where to do that
+//When called it should keep adding the array and the guessessofar
+//var guessedSoFar = function(){
+//  document.querySelector("#guessessofar").innerHTML = userGuess;
+//  }  
+
+//reset - Empyting out the guessed function to allow it run again????
+//global.guessed = function(){}
 
 
+//wins
+//var won = function(){
+// document.querySelector("#wins").innerHTML = wins;
 
 
-//updateguesesssofar
+//lost
+//var lost = function(){
+//  document.querySelector("#losses").innerHTML = losses;
+  
 
 
-
-
-
-//reset
 
 
 //Execute the functions here
 guessed()
 
-
-
-
-
+//won()
+//lost()
 
 //When user clicks a button 
 document.onkeyup = function(event) {
 
 //automatically decreases the amount of guesses
 //-- subtracts
-// ++ adds
-  guessesLeft--;
-  //executes the guessed function
-guessed();
+// ++ adds/
+  guessesLeft--;  
+  guessed();     //executes the guessed function
+ 
+//  wins++     should execute the won function
+//   won()
 
 //captures teh user guess and translates to lowercase
-  var userGuess = String.fromCharCode(event.keycode).toLowerCase();
+var userGuess = String.fromCharCode(event.keycode).toLowerCase();
    
   console.log(userGuess);
 }
+
